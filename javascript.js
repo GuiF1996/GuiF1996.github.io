a608069d23e4d4fad9fb14398c1aae80b7c5ac41
GuiF1996 <Guillaume.fleurisson@gmail.com>
2017-03-10 14:17:26 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -11,7 +11,7 @@ xmlhttp.send();*/
 
 $(document).ready(function() {
 	$.ajax({
-		url : "http://bitcoin.mubiz.com/blocks",
+		url : "http://bitcoin.mubiz.com/blockchaininfo",
 		dataType : "json",
 		contentType : "application/json; charset=utf-8",
 		type : "GET",
